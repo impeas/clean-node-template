da5ecad51b9adf2fabd4baacf6f0841a397766ac
Michał Walczuk <michal.walczuk@ext.denkwerk.com>
2024-06-27 20:41:16 +0200

    add basic node + ts template

---
+++ server.ts
@@ -0,0 +1,12 @@
+import http from "http";
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(
+    JSON.stringify({
+      data: "hello world",
+    })
+  );
+});
+
+server.listen(3005);
